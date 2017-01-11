29181f55488e7fdacbe7066d09c92aef2500f7e3
tbaddade <thomas@redaxo.org>
2017-01-11 20:50:38 +0100

    MBlock berücksichtigen

--- assets/iconpicker.js
+++ assets/iconpicker.js
@@ -1,39 +1,61 @@
-$(document).on('rex:ready', function (event, container) {
 
-    var IconpickerAddon = function ($) {
-        return {
+(function ($) {
+    var IconpickerAddon = {};
 
-            init: function () {
-                var body = $('body');
-                IconpickerAddon.initMainContent(body);
-            },
+    IconpickerAddon.init = function () {
+        var body = $('body');
+        IconpickerAddon.initMainContent(body);
+    },
 
-            initContent: function(content) {
-                content.find('[data-iconpicker]').each(function () {
-                    var input = $(this);
-                    var button = $('<button type="button" class="btn btn-default" name="' + input.attr('name') + '">Iconpicker</button>');
-                    var iconpickerset = input.attr('data-iconpicker') || 'fontawesome';
+    IconpickerAddon.initContent = function(content) {
+        content.find('[data-iconpicker]').each(function () {
+            var input = $(this);
+            var iconpickerset = input.attr('data-iconpicker') || 'fontawesome';
+            var button = $('<button type="button" class="btn btn-default" name="' + input.attr('name') + '" data-iconpickerset="' + iconpickerset + '">Iconpicker</button>');
 
-                    button.iconpicker({
-                        arrowPrevIconClass: "rex-icon rex-icon-previous",
-                        arrowNextIconClass: "rex-icon rex-icon-next",
-                        iconset: $['iconset_' + iconpickerset],
-                        icon: input.val(),
-                        placement: 'right',
-                        cols: 20,
-                        rows: 10
-                    });
-                    input.parent().append($('<div>').append(button));
-                    input.remove();
-                });
-            },
+            button.iconpicker({
+                arrowPrevIconClass: "rex-icon rex-icon-previous",
+                arrowNextIconClass: "rex-icon rex-icon-next",
+                iconset: $['iconset_' + iconpickerset],
+                icon: input.val(),
+                placement: 'right',
+                cols: 20,
+                rows: 10
+            });
+            // input.parent().append($('<div>').append(button));
+            input.parent().append(button);
+            input.remove();
+        });
+    },
 
-            initMainContent: function(content) {
-                IconpickerAddon.initContent(content);
-            }
-        };
+    IconpickerAddon.initMblock = function(content) {
+        content.find('.iconpicker').each(function () {
+            var button = $(this);
+            var iconpickerset = button.attr('data-iconpicker') || 'fontawesome';
 
-    }(jQuery);
+            button.iconpicker({
+                arrowPrevIconClass: "rex-icon rex-icon-previous",
+                arrowNextIconClass: "rex-icon rex-icon-next",
+                iconset: $['iconset_' + iconpickerset],
+                icon: '',
+                placement: 'right',
+                cols: 20,
+                rows: 10
+            });
+        });
+    },
 
-    IconpickerAddon.init();
-});
+    IconpickerAddon.initMainContent = function(content) {
+        IconpickerAddon.initContent(content);
+    }
+
+    $(document).on('rex:ready', function(event, container) {
+        IconpickerAddon.init();
+    });
+
+    mblock_module.registerCallback('reindex_end', function() {
+        IconpickerAddon.initMblock($('.mblock_wrapper'));
+    });
+
+}(jQuery));
+
