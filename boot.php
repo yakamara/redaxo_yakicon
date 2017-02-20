10cce0160b5076c9bc251de06a712bb11d623e51
tbaddade <thomas@redaxo.org>
2017-02-20 11:35:58 +0100

    Fontawesome auf 4.7.0 aktualisiert

--- boot.php
+++ boot.php
@@ -4,12 +4,10 @@ if (rex::isBackend()) {
     // rex_view::addCssFile($this->getAssetsUrl('fonts/icomoon/styles.css'));
     // rex_view::addJsFile($this->getAssetsUrl('iconsets/iconset-icomoon.js'));
 
-    rex_view::addCssFile($this->getAssetsUrl('fonts/font-awesome-4.6.3/css/font-awesome.min.css'));
-    rex_view::addJsFile($this->getAssetsUrl('iconsets/iconset-font-awesome-4.6.3.js'));
+    rex_view::addCssFile($this->getAssetsUrl('fonts/font-awesome-4.7.0/css/font-awesome.min.css'));
+    rex_view::addJsFile($this->getAssetsUrl('iconsets/iconset-font-awesome-4.7.0.js'));
 
     rex_view::addCssFile($this->getAssetsUrl('vendor/bootstrap-iconpicker/bootstrap-iconpicker/css/bootstrap-iconpicker.min.css'));
-    //rex_view::addJsFile($this->getAssetsUrl('vendor/bootstrap-iconpicker/bootstrap-iconpicker/js/iconset/iconset-fontawesome-4.2.0.min.js'));
-    //rex_view::addJsFile($this->getAssetsUrl('vendor/bootstrap-iconpicker/bootstrap-iconpicker/js/bootstrap-iconpicker.min.js'));
     rex_view::addJsFile($this->getAssetsUrl('vendor/bootstrap-iconpicker/bootstrap-iconpicker/js/bootstrap-iconpicker.js'));
     rex_view::addJsFile($this->getAssetsUrl('iconpicker.js'));
 }
